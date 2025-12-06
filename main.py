94812a0f4fca13b568ed3bcf050d64962d1a21a0
Wyatt Johnson <enderprooffical@gmail.com>
2025-12-05 22:30:39 -0500

    Added logging

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import glob as glob
 
+# Create logging file (Just writing to file instead of using logging package for simplicity)
+log_file = open("log_file.txt", "w")
+
+def add_file_to_log(file):
+    with open("log_file.txt", "a") as l:
+        l.write(file)
+
 photos = glob.glob('**/*.png' or '**/*.jpg' or '**/*.jpeg' or '**/*.svg')
 videos = glob.glob('**/*.mp4' or '**/*.mov')
 programs = glob.glob('**/*.py' or '**/*.js')
@@ -8,15 +15,20 @@ audios = glob.glob('**/*.mp3' or '**/*.wav')
 print('\nPhotos:')
 for photo in photos:
     print(photo)
+    add_file_to_log(photo)
 
 print('\nVideos:')
 for video in videos:
     print(video)
+    add_file_to_log(video)
 
 print('\nPrograms:')
 for program in programs:
     print(program)
+    add_file_to_log(program)
 
 print('\nAudio:')
 for audio in audios:
     print(audio)
+    add_file_to_log(audio)
+
