aa2fadb79433960739179b02da7db72c5323bc3d
Wyatt Johnson <wyatt@Lea.localdomain>
2025-12-10 16:40:02 -0500

    Added line-breaks to log file

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ log_file = open("log_file.txt", "w")
 
 
 def add_file_to_log(file):
-    with open("log_file.txt", "a") as l:
-        l.write(file)
+    with open("log_file.txt", "a") as log:
+        log.write(file)
 
 
 def find_files():
@@ -20,28 +20,38 @@ def find_files():
     audios = glob.glob("**/*.mp3" or "**/*.wav")
 
     print("\nPhotos:")
+    with open("log_file.txt", "a") as log:
+        log.write('Photos:\n')
     for photo in photos:
         print(photo)
         add_file_to_log(photo)
         files_found += 1
 
     print("\nVideos:")
+    with open("log_file.txt", "a") as log:
+        log.write('\n\nVideos:\n')
     for video in videos:
         print(video)
         add_file_to_log(video)
         files_found += 1
 
     print("\nPrograms:")
+    with open("log_file.txt", "a") as log:
+        log.write('\n\nPrograms:\n')
     for program in programs:
         print(program)
         add_file_to_log(program)
         files_found += 1
 
     print("\nAudio:")
+    with open("log_file.txt", "a") as log:
+        log.write('\n\nAudio:\n')
     for audio in audios:
         print(audio)
         add_file_to_log(audio)
         files_found += 1
+    with open("log_file.txt", "a") as log:
+        log.write('\n')
 
     end_time = time.time()
     time_taken = end_time - start_time
